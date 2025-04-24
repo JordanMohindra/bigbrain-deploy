b75b20c67b414fa0a904737271d9673d1e09c290
Jordan <z5418455@ad.unsw.edu.au>
2025-04-24 14:22:43 +1000

    changes to frontend to hopefully get deployment working

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5005"
+const API_URL = "https://bigbrain-deploy-wmdm.vercel.app"
 
 // Create axios instance
 const api = axios.create({
